cbc41b279be93634db5f436279b6ce6f862c889b
Kevin Black <kevin3.black@gmail.com>
2021-06-06 17:37:28 -0500

    Stuff

--- robot-pushing/push_env.py
+++ robot-pushing/push_env.py
@@ -33,31 +33,36 @@ class PushingEnvironment(gym.Env):
         low, high = self.env.action_spec
         self.action_space = spaces.Box(low=low[:3], high=high[:3])
 
-        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=[12])
+        self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=[6])
         self.curr_obs = None
         self.step_num = None
+        self.goal_pos = None
 
     def seed(self, seed=None):
         if seed is not None:
             np.random.seed(seed)
             self.action_space.seed(seed)
 
-    def _get_flat_obs(self, obs):
+    def _get_flat_obs(self, obs, goal_pos):
         return np.concatenate([
             obs["robot0_eef_pos"],
-            obs["gripper_to_cube_pos"],
-            obs["gripper_to_goal_pos"],
-            obs["cube_to_goal_pos"],
+            obs["robot0_eef_pos"] - goal_pos,
         ])
 
     def reset(self):
         self.curr_obs = self.env.reset()
         self.step_num = 0
-        return self._get_flat_obs(self.curr_obs)
+        self.goal_pos = self.curr_obs["goal_pos"]
+        self.goal_pos[2] = np.random.uniform(
+            self.env.GRIPPER_BOUNDS_MIN[2] + self.env.table_offset[2] + 0.025,
+            self.env.GRIPPER_BOUNDS_MAX[2] + self.env.table_offset[2] - 0.025,
+        )
+        return self._get_flat_obs(self.curr_obs, self.goal_pos)
 
     def step(self, action):
-        next_obs, reward, done, info = self.env.step(np.concatenate([action, [0, 0, 0]]))
-        return_obs = self._get_flat_obs(next_obs)
+        next_obs, _, done, info = self.env.step(np.concatenate([action, [0, 0, 0]]))
+        return_obs = self._get_flat_obs(next_obs, self.goal_pos)
+        reward = self.compute_reward(next_obs["robot0_eef_pos"], self.goal_pos, info)
         if self.renderable:
             info["image"] = self.curr_obs["frontview_image"][::-1]
             info["step"] = self.step_num
@@ -74,18 +79,10 @@ class PushingEnvironment(gym.Env):
         """
         obs = np.array(obs)
         obs_next = np.array(obs_next)
-        # fake_goal = obs_next[-1, :3]
-        # obs[:, 6:9] = obs[:, :3] - fake_goal
-        # obs_next[:, 6:9] = obs_next[:, :3] - fake_goal
-        # final cube position
-        fake_goal = obs_next[-1, :3] - obs_next[-1, 3:6]
-        # gripper to goal pos
-        obs[:, 6:9] = obs[:, :3] - fake_goal
-        obs_next[:, 6:9] = obs_next[:, :3] - fake_goal
-        # cube to goal pos
-        obs[:, 9:] = (obs[:, :3] - obs[:, 3:6]) - fake_goal
-        obs_next[:, 9:] = (obs_next[:, :3] - obs_next[:, 3:6]) - fake_goal
-        rewards = [self.env.compute_reward(fake_goal, on[:3] - on[3:6], {}) for on in obs_next]
+        fake_goal = obs_next[-1, :3]
+        obs[:, 3:] = obs[:, :3] - fake_goal
+        obs_next[:, 3:] = obs_next[:, :3] - fake_goal
+        rewards = [self.compute_reward(on[:3], fake_goal, {}) for on in obs_next]
         # rewards = []
         # for on in obs_next:
         #     reward = self.compute_reward(fake_goal, on[:3] - on[3:6], {})
@@ -95,6 +92,9 @@ class PushingEnvironment(gym.Env):
         dones = np.full_like(rewards, False, dtype=bool)
         dones[-1] = True
         return obs[:len(rewards)], obs_next[:len(rewards)], np.array(rewards), dones
+
+    def compute_reward(self, gripper_pos, goal_pos, info):
+        return 0 if np.linalg.norm(gripper_pos - goal_pos) <= 0.025 else -1
 
     def render(self, mode="human"):
         assert self.renderable
